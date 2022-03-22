f2870fdcdc175c78b63078f99095fb6730aa219a
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2022-03-22 14:42:21 +0900

    get most of the tutorial done

---
+++ pages/api/comments.ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import {GraphQLClient, gql } from 'graphql-request';
+type Data = {
+  name: string,
+  email: string,
+  comment: string,
+  post: { connect: {slug: string} }
+}
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+
+export default async function comments(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log({graphcmsToken});
+
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    }
+  });
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!){
+      createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: {slug: $slug}}}) { id }
+    }
+  `;
+  try {
+    const result = await graphQLClient.request(query, {
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment,
+      slug: req.body.slug,
+    })
+    return res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+}
